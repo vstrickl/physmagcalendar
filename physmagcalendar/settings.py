b3c69e1d0f869e30d7a3e732ef66817d0d6b13e5
Vonique Stricklen <vstrickl.git@gmail.com>
2023-01-02 09:05:33 -0800

    Collecting Static

--- physmagcalendar/settings.py
+++ physmagcalendar/settings.py
@@ -126,13 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Google Calendar APIs
 
