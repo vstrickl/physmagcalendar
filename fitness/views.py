567005dc2110ed2853112a046983534cb9034494
Vonique Stricklen <vstrickl.git@gmail.com>
2023-01-02 01:39:28 -0800

    Fixing Bug

--- fitness/views.py
+++ fitness/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.conf import settings
 
 # Create your views here.
 
@@ -7,8 +6,6 @@ def fitness(request):
 
     context = { 
         'title' : 'Fitness',
-        'fitnessid' : settings.FITNESS_ID,
-        'apikey' : settings.GOOGLE_API_KEY,
     }
 
     return render(request, 'fitness.html', context)
