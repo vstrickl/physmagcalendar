04d180737990b7b30f02bf722242d214ea5b8a5a
Vonique Stricklen <vstrickl.git@gmail.com>
2025-07-21 14:47:32 -0700

    fixed NoneType string issue

--- app/boxing/models.py
+++ app/boxing/models.py
@@ -10,6 +10,6 @@ class BoxingClass(models.Model):
     day_time = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
-        if self.class_name.name.exists():
-            return f"{self.class_name.name}: {self.day_time}"
-        return self.day_time
+        class_names = ", ".join(c.name for c in self.class_name.all())  # pylint: disable=no-member
+        day_time_str = str(self.day_time) if self.day_time else ""
+        return f"{class_names}: {day_time_str}" if class_names else day_time_str
