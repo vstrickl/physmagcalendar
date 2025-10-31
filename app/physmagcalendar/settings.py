eba0c81a80bcd05524e501619e89ea642dd76a51
Vonique Stricklen <vstrickl.git@gmail.com>
2025-10-31 15:21:17 -0700

    Fixing image schedule cloudinary bug

--- app/physmagcalendar/settings.py
+++ app/physmagcalendar/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+
+import cloudinary
+
 from decouple import config,  UndefinedValueError
 from django.core.exceptions import ImproperlyConfigured
 
@@ -188,19 +191,16 @@ CACHES = {
 }
 
 # Cloudinary
+
 CLOUD_NAME = config("CLOUD_NAME", default=None)
 CLOUD_API_KEY = config("CLOUD_API_KEY", default=None)
 CLOUD_API_SECRET = config("CLOUD_API_SECRET", default=None)
 
-if CLOUD_NAME and CLOUD_API_KEY and CLOUD_API_SECRET:
-    CLOUDINARY = {
-        "cloud_name": CLOUD_NAME,
-        "api_key": CLOUD_API_KEY,
-        "api_secret": CLOUD_API_SECRET,
-    }
-else:
-    # Safe fallback so build doesn't crash
-    CLOUDINARY = {}
+cloudinary.config(
+    cloud_name=CLOUD_NAME,
+    api_key=CLOUD_API_KEY,
+    api_secret=CLOUD_API_SECRET,
+)
 
 # Maximum size for uploaded files (in bytes)
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
